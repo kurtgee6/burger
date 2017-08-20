668eac7767ac8c6909c732e31dc3f6fe7fd54f48
Kurt Griarte <kurtgee11@gmail.com>
2017-08-20 13:08:51 -0400

    schema and seed sql's done

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burger_db;
+
+INSERT INTO burger (name, devour) VALUES ('Cheeseburger', true);
+INSERT INTO burger (name, devour) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burger (name, devour) VALUES ('Double Cheeseburger', true);
+INSERT INTO burger (name, devour) VALUES ('Veggie Burger', true);
+INSERT INTO burger (name, devour) VALUES ('Regular Burger', true);
+INSERT INTO burger (name, devour) VALUES ('Kobe Burger', true);
+INSERT INTO burger (name, devour) VALUES ('Onion Burger', true);
