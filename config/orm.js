4cea78660c36332cc229a33f3c228aed26692926
Kurt Griarte <kurtgee11@gmail.com>
2017-08-22 18:27:53 -0400

    finished ORM

---
+++ config/orm.js
@@ -0,0 +1,71 @@
+var connection = require("./connection.js");
+
+// Object for all our SQL statement functions.
+var orm = {
+
+    all: function (table, cb) {
+        var queryString = "SELECT * FROM " + table + ';';
+        connection.query(queryString, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestion(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+
+    },
+
+    delete: function (table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+
+};
+
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,1 @@
+var orm = require("../config/burger_controller.js");
